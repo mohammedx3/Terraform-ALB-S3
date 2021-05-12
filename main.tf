1f5ccf43b457a59695542ae899a9e28e100bdd37
Mohammed Hassan <mohammedx3@hotmail.com>
2021-05-13 01:54:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-  region = "eu-west-1"
-  profile = "default"
-  shared_credentials_file = "~/.aws/credentials"
-}
+# provider "aws" {
+#   region = "eu-west-1"
+#   profile = "default"
+#   shared_credentials_file = "~/.aws/credentials"
+# }
 
 
 

56a6afd77ab09b0321f46f38ba202716f0b6056a
Mohammed Hassan <mohammedx3@hotmail.com>
2021-05-13 02:02:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-west-1"
-  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
+  secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
 }
 
 

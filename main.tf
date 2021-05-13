c32172bd5d21d14719b3620b2f28fc4be918d723
mohammedx3 <mohammedx3@hotmail.com>
2021-05-13 04:49:33 +0200

    comments

--- main.tf
+++ main.tf
@@ -1,61 +1,65 @@
+// Speicfy AWS region
 provider "aws" {
-  region = "eu-west-1"
-#   access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-#   secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+  region = "eu-west-1" 
+
 }
 
-
-
+// Create 2 text files and import timestamp in them
 resource "local_file" "to_dir" {
   count    = "${length(local.source_files)}"
   filename = "${path.module}/createdFiles/${basename(element(local.source_files, count.index))}"
   content  = "${local.timestamp}"
 }
 
+// Specify variable with s3 bucket name
 variable "bucket_name" {
   description = "The name of the bucket"
   default     = "terratest-task-s3"
 }
 
-
+// Set local timestamp to be imported in the 2 files.
 locals {
   source_files = ["${path.module}/createdFiles/file1.txt", "${path.module}/createdFiles/file2.txt"]
   timestamp = "${timestamp()}"
 }
 
 
+// Create private s3 bucket
 resource "aws_s3_bucket" "terratest-bucket" {
   bucket = "${var.bucket_name}"
-  acl    = "public-read"
+  # acl    = "public-read"
+    acl    = "private"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
 
-
+// Import file1 into the bucket but wait for the bucket to be created first.
 resource "aws_s3_bucket_object" "object1" {
   bucket = "${var.bucket_name}"
   key    = "file1.txt"
-  acl = "public-read"
+  # acl = "public-read"
+    acl = "private"
   source = "${path.module}/createdFiles/file1.txt"
   depends_on = [
     aws_s3_bucket.terratest-bucket
   ]
 }
 
-
+// Import file2 into the bucket but wait for the bucket to be created first.
 resource "aws_s3_bucket_object" "object2" {
   bucket = "${var.bucket_name}"
   key    = "file2.txt"
-  acl = "public-read"
+  # acl = "public-read"
+    acl = "private"
   source = "${path.module}/createdFiles/file2.txt"
-  depends_on = [
+  depends_on = [ 
     aws_s3_bucket.terratest-bucket
   ]
 }
 
-
+// Export bucket id to be later used in terratest.
 output "bucket_id" {
   value = aws_s3_bucket.terratest-bucket.id
 }
--- test/terraform_test.go
+++ test/terraform_test.go
@@ -1,30 +1,23 @@
 package test
 
 import (
-//   "fmt"
-//   "strings"
   "testing"
-
   "github.com/gruntwork-io/terratest/modules/aws"
-//   "github.com/gruntwork-io/terratest/modules/random"
   "github.com/gruntwork-io/terratest/modules/terraform"
   "github.com/stretchr/testify/assert"
 )
 
 // Standard Go test, with the "Test" prefix and accepting the *testing.T struct.
 func TestS3Bucket(t *testing.T) {
-  // I work in eu-west-2, you may differ
+  // Set AWS region.
   awsRegion := "eu-west-1"
 
   // This is using the terraform package that has a sensible retry function.
   terraformOpts := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-    // Our Terraform code is in the /aws folder.
+    // Our Terraform code is in the /terraformTask folder.
     TerraformDir: "../",
-
-    // This allows us to define Terraform variables. We have a variable named
-    // "bucket_name" which essentially is a suffix. Here we are are using the
-    // random package to get a unique id we can use for testing, as bucket names
-    // have to be unique.
+    
+    // This allows us to define Terraform variables. We have a variable named "bucket_name" to use it in our testing.
     Vars: map[string]interface{}{
       "bucket_name": "terratest-task-s3",
     },
@@ -35,7 +28,7 @@ func TestS3Bucket(t *testing.T) {
     },
   })
 
-  // We want to destroy the infrastructure after testing.
+  // To destroy the infrastructure after testing.
 //   defer terraform.Destroy(t, terraformOpts)
 
   // Deploy the infrastructure with the options defined above
@@ -44,13 +37,14 @@ func TestS3Bucket(t *testing.T) {
   // Get the bucket ID so we can query AWS
   bucketID := terraform.Output(t, terraformOpts, "bucket_id")
 
-
+  // Test that the bucket exists.
   actualBucketStatus := aws.AssertS3BucketExistsE(t, awsRegion, bucketID)
+  assert.Equal(t, nil, actualBucketStatus)
+  // Test there is 2 files with names "file1.txt" and "file2.txt"
   actualBucketObject1Content, _ := aws.GetS3ObjectContentsE(t, awsRegion, bucketID, "file1.txt")
   actualBucketObject2Content, _ := aws.GetS3ObjectContentsE(t, awsRegion, bucketID, "file2.txt")
-  
-  // Test that the status we get back from AWS is "Enabled" for versioning
-  assert.Equal(t, nil, actualBucketStatus)
   assert.NotEqual(t, nil, actualBucketObject1Content)
   assert.NotEqual(t, nil, actualBucketObject2Content)
+
+
 }

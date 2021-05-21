d5ff6bb2efc019748d5886e41790924fc17e8343
mohammedx3 <mohammedx3@hotmail.com>
2021-05-21 14:28:59 +0200

    all working

--- main.tf
+++ main.tf
@@ -328,7 +328,6 @@ resource "aws_security_group" "lb_sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {

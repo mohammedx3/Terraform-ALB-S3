9bf0fd173ef801e6ff8931d704d6bb7ae0eb8ca8
mohammedx3 <mohammedx3@hotmail.com>
2021-05-22 13:09:02 +0200

    added tests

--- test/terraform_test.go
+++ test/terraform_test.go
@@ -1,9 +1,9 @@
 package test
 
 import (
-	// "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 	"fmt"
-	// "strings"
+	"strings"
 	"testing"
 
 	"encoding/json"
@@ -45,97 +45,97 @@ func LoadConfigFile() Configuration {
 }
 
 // Standard Go test, with the "Test" prefix and accepting the *testing.T struct.
-// func TestS3Bucket(t *testing.T) {
-// 	// Load configuration file
-// 	config := LoadConfigFile()
+func TestS3Bucket(t *testing.T) {
+	// Load configuration file
+	config := LoadConfigFile()
 
-// 	// This is using the terraform package that has a sensible retry function.
-// 	terraformOpts := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-// 		// Our Terraform code is in the /terraformTask folder.
-// 		TerraformDir: config.TERRAFORM_DIR,
+	// This is using the terraform package that has a sensible retry function.
+	terraformOpts := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Our Terraform code is in the /terraformTask folder.
+		TerraformDir: config.TERRAFORM_DIR,
 
-// 		// This allows us to define Terraform variables. We have a variable named "bucket_name" to use it in our testing.
-// 		Vars: map[string]interface{}{
-// 			"bucket_name": fmt.Sprintf("%v", strings.ToLower(random.UniqueId())),
-// 		},
+		// This allows us to define Terraform variables. We have a variable named "bucket_name" to use it in our testing.
+		Vars: map[string]interface{}{
+			"bucket_name": fmt.Sprintf("%v", strings.ToLower(random.UniqueId())),
+		},
 
-// 		// Setting the environment variables, specifically the AWS region.
-// 		EnvVars: map[string]string{
-// 			"AWS_DEFAULT_REGION": config.REGION,
-// 		},
-// 	})
+		// Setting the environment variables, specifically the AWS region.
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": config.REGION,
+		},
+	})
 
-// 	// To destroy the infrastructure after testing.
-// 	defer terraform.Destroy(t, terraformOpts)
+	// To destroy the infrastructure after testing.
+	defer terraform.Destroy(t, terraformOpts)
 
-// 	// Deploy the infrastructure with the options defined above
-// 	terraform.InitAndApply(t, terraformOpts)
+	// Deploy the infrastructure with the options defined above
+	terraform.InitAndApply(t, terraformOpts)
 
-// 	// Get the bucket ID so we can query AWS
-// 	bucketID := terraform.Output(t, terraformOpts, "bucket_id")
+	// Get the bucket ID so we can query AWS
+	bucketID := terraform.Output(t, terraformOpts, "bucket_id")
 
-// 	// Test that the bucket exists.
-// 	actualBucketStatus := aws.AssertS3BucketExistsE(t, config.REGION, bucketID)
-// 	assert.Equal(t, nil, actualBucketStatus)
+	// Test that the bucket exists.
+	actualBucketStatus := aws.AssertS3BucketExistsE(t, config.REGION, bucketID)
+	assert.Equal(t, nil, actualBucketStatus)
 
-// 	// Test there is 2 files with names "file1.txt" and "file2.txt"
-// 	actualBucketObject1Content, _ := aws.GetS3ObjectContentsE(t, config.REGION, bucketID, "test1.txt")
-// 	actualBucketObject2Content, _ := aws.GetS3ObjectContentsE(t, config.REGION, bucketID, "testt2.txt")
+	// Test there is 2 files with names "file1.txt" and "file2.txt"
+	actualBucketObject1Content, _ := aws.GetS3ObjectContentsE(t, config.REGION, bucketID, "test1.txt")
+	actualBucketObject2Content, _ := aws.GetS3ObjectContentsE(t, config.REGION, bucketID, "testt2.txt")
 
-// 	// Assert files were uploaded by checking returned content is not null and of type strings
-// 	assert.NotEqual(t, nil, actualBucketObject1Content)
-// 	assert.IsType(t, "", actualBucketObject1Content)
-// 	assert.NotEqual(t, nil, actualBucketObject2Content)
-// 	assert.IsType(t, "", actualBucketObject2Content)
-// }
+	// Assert files were uploaded by checking returned content is not null and of type strings
+	assert.NotEqual(t, nil, actualBucketObject1Content)
+	assert.IsType(t, "", actualBucketObject1Content)
+	assert.NotEqual(t, nil, actualBucketObject2Content)
+	assert.IsType(t, "", actualBucketObject2Content)
+}
 
-// func TestTerraformAwsNetwork(t *testing.T) {
-// 	t.Parallel()
+func TestTerraformAwsNetwork(t *testing.T) {
+	t.Parallel()
 	
-// 	// Load configuration file
-// 	config := LoadConfigFile()
+	// Load configuration file
+	config := LoadConfigFile()
 
 	
-// 	// Give the VPC and the subnets correct CIDRs
-// 	vpcCidr := "10.0.0.0/16"
-// 	publicSubnetCidr1  := "10.0.0.0/24"
-// 	publicSubnetCidr2 := "10.0.1.0/24"
-// 	awsRegion := "eu-west-1"
+	// Give the VPC and the subnets correct CIDRs
+	vpcCidr := "10.0.0.0/16"
+	publicSubnetCidr1  := "10.0.0.0/24"
+	publicSubnetCidr2 := "10.0.1.0/24"
+	awsRegion := "eu-west-1"
 
-// 	// Construct the terraform options with default retryable errors to handle the most common retryable errors in
-// 	// terraform testing.
-// 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-// 		// The path to where our Terraform code is located
-// 		TerraformDir: config.TERRAFORM_DIR,
+	// Construct the terraform options with default retryable errors to handle the most common retryable errors in
+	// terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: config.TERRAFORM_DIR,
 
-// 		// Variables to pass to our Terraform code using -var options
-// 		Vars: map[string]interface{}{
-// 			"main_vpc_cidr":       vpcCidr,
-// 			"first_subnet_cidr": publicSubnetCidr1,
-// 			"second_subnet_cidr":  publicSubnetCidr2,
-// 			"aws_region":          awsRegion,
-// 		},
-// 	})
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{
+			"main_vpc_cidr":       vpcCidr,
+			"first_subnet_cidr": publicSubnetCidr1,
+			"second_subnet_cidr":  publicSubnetCidr2,
+			"aws_region":          awsRegion,
+		},
+	})
 
-// 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-// 	defer terraform.Destroy(t, terraformOptions)
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
 
-// 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-// 	terraform.InitAndApply(t, terraformOptions)
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
 
-// 	// Run `terraform output` to get the value of an output variable
-// 	publicSubnetId1 := terraform.Output(t, terraformOptions, "public1_subnet_id")
-// 	publicSubnetId2 := terraform.Output(t, terraformOptions, "public2_subnet_id")
-// 	vpcId := terraform.Output(t, terraformOptions, "main_vpc_id")
+	// Run `terraform output` to get the value of an output variable
+	publicSubnetId1 := terraform.Output(t, terraformOptions, "public1_subnet_id")
+	publicSubnetId2 := terraform.Output(t, terraformOptions, "public2_subnet_id")
+	vpcId := terraform.Output(t, terraformOptions, "main_vpc_id")
 
-// 	subnets := aws.GetSubnetsForVpc(t, vpcId, awsRegion)
+	subnets := aws.GetSubnetsForVpc(t, vpcId, awsRegion)
 
-// 	require.Equal(t, 2, len(subnets))
-// 	// Verify if the network that is supposed to be public is really public
-// 	assert.True(t, aws.IsPublicSubnet(t, publicSubnetId1, awsRegion))
-// 	// Verify if the network that is supposed to be public is really public
-// 	assert.True(t, aws.IsPublicSubnet(t, publicSubnetId2, awsRegion))
-// }
+	require.Equal(t, 2, len(subnets))
+	// Verify if the network that is supposed to be public is really public
+	assert.True(t, aws.IsPublicSubnet(t, publicSubnetId1, awsRegion))
+	// Verify if the network that is supposed to be public is really public
+	assert.True(t, aws.IsPublicSubnet(t, publicSubnetId2, awsRegion))
+}
 
 func TestTerraformAwsExample(t *testing.T) {
 	t.Parallel()
